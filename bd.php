93b2c66d06952b4a3caac5cb226c59cbb3d02ace
Datc0601 <99293417+Datc0601@users.noreply.github.com>
2024-09-11 15:44:18 -0500

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,6 +1,30 @@
 <?php
 
-$conexion=mysqli_connect("localhost","appreuniones","123456","reuniones" )or die("error conexion");
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+include('bd.php');
+
+$numeroacta=$_POST['txtNumero'];
+$lugar=$_POST['txtLugar'];
+$fecha=$_POST['txtFecha'];
+$horaI=$_POST['txtHoraI'];
+$horaF=$_POST['txtHoraF'];
+$covocador=$_POST['txtConvocador'];
+$tematica=$_POST['txtTematica'];
+
+
+$consulta="INSERT INTO `personal` ( `Número de acta`, `Lugar`, `Fecha`, `HoraI`, `HoraF`, `Convocador`, `Tematica`)
+ VALUES ('$numeroacta', '$lugar', '$fecha', '$horaI', '$horaF', '$covocador', '$tematica')";
+
+$resultado=mysqli_query($conexion,$consulta) or die("error de registro");
+
+echo "registro exitoso";
+
+mysqli_close($conexion);
+
+
+
 
 
 ?>
